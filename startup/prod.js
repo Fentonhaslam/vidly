c5fe9d31f2ecd44768dac3cd84e77e8c435aa604
fentonhaslam <fentonhaslam@gmail.com>
2020-09-15 22:45:26 +0100

    fixed exports issue

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,7 @@
 const helmet = require("helmet");
 const compression = require("compression");
 
-module.export = function (app) {
+module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
 };
